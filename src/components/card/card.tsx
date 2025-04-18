11a4bec1e44144ed7b41e337247a44171f222a56
lenapokrovskaya <lena.pokrovskaya@yahoo.com>
2025-04-19 00:06:06 +0300

    Создает 404 страницу, подключает и настраивает маршрутизацию

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,4 +1,4 @@
-
+import { Link } from 'react-router-dom';
 
 function Card(): JSX.Element {
   return (
@@ -7,9 +7,9 @@ function Card(): JSX.Element {
         <span>Premium</span>
       </div>
       <div className="cities__image-wrapper place-card__image-wrapper">
-        <a href="#">
+        <Link to="#">
           <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"/>
-        </a>
+        </Link>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
@@ -31,7 +31,7 @@ function Card(): JSX.Element {
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+          <Link to="#">Beautiful &amp; luxurious apartment at great location</Link>
         </h2>
         <p className="place-card__type">Apartment</p>
       </div>
