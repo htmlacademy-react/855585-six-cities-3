d6f6398f9a64ad3b51b452c31ce811e06d13a000
lenapokrovskaya <lena.pokrovskaya@yahoo.com>
2025-04-29 16:13:46 +0300

    Рефакторинг (вынесение вспомогательной функции, деструктруизация)

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,15 +1,15 @@
 import { Link } from 'react-router-dom';
 import { Offers } from '../../types/offers';
 import { AppRoute } from '../../const';
+import { capitalize } from '../../utils';
 
 type CardProps = {
   offers: Offers;
   handleHover: (offer?: Offers) => void;
 }
 
-const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
-
 function Card({offers, handleHover}: CardProps): JSX.Element {
+  const {id, isPremium, previewImage, price, title, type} = offers;
   const handleMouseOn = () => {
     handleHover(offers);
   };
@@ -18,23 +18,23 @@ function Card({offers, handleHover}: CardProps): JSX.Element {
     handleHover();
   };
   return (
-    <Link to={`${AppRoute.Offer}/${offers.id}`}>
+    <Link to={`${AppRoute.Offer}/${id}`}>
       <article
         className="cities__card place-card"
         onMouseEnter={handleMouseOn}
         onMouseLeave={handleMouseOff}
       >
-        {offers.isPremium &&
+        {isPremium &&
           <div className="place-card__mark">
             <span>Premium</span>
           </div>}
         <div className="cities__image-wrapper place-card__image-wrapper">
-          <img className="place-card__image" src={offers.previewImage}/>
+          <img className="place-card__image" src={previewImage}/>
         </div>
         <div className="place-card__info">
           <div className="place-card__price-wrapper">
             <div className="place-card__price">
-              <b className="place-card__price-value">&euro;{offers.price}</b>
+              <b className="place-card__price-value">&euro;{price}</b>
               <span className="place-card__price-text">&#47;&nbsp;night</span>
             </div>
             <button className="place-card__bookmark-button button" type="button">
@@ -50,8 +50,8 @@ function Card({offers, handleHover}: CardProps): JSX.Element {
               <span className="visually-hidden">Rating</span>
             </div>
           </div>
-          <h2 className="place-card__name">{offers.title}</h2>
-          <p className="place-card__type">{capitalize(offers.type)}</p>
+          <h2 className="place-card__name">{title}</h2>
+          <p className="place-card__type">{capitalize(type)}</p>
         </div>
       </article>
     </Link>
